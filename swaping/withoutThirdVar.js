23a2a0bab05eef1fbba6cb2465e50b18c10cb387
Tinkal Kumar <tinkalkumar67693@gmail.com>
2025-05-05 09:54:52 +0530

    swap without extra variable

---
+++ swaping/withoutThirdVar.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 20;
+
+a = a + b; // 10+20 = 30 now a =30
+b = a - b; // 30 - 20 = 10
+a = a - b; // 30 - 10 = 20
+
+console.log(a, b);
+
+let c = 44;
+let d = 64;
+
+c = c + d; // 44+64 = 108
+d = c - d; // 108 - 64 = 44
+c = c - d; // 108 - 44 = 64
+
+console.log(d, c);
