6711c50d2365e688def58c58e396625829121f82
Tinkal Kumar <tinkalkumar67693@gmail.com>
2026-03-15 20:31:02 +0530

    filter

---
+++ Array/filter.js
@@ -0,0 +1,48 @@
+// // // // // let numbers = [1,2,3,4,5,6,7,8];
+
+// // // // // let newarr = numbers.filter(num => num%2 === 0)
+
+// // // // // console.log(newarr)
+
+// // // // let users = [
+// // // //   { id: 1, name: "Rahul", age: 22 },
+// // // //   { id: 2, name: "Amit", age: 25 },
+// // // //   { id: 3, name: "Neha", age: 24 },
+// // // //   { id: 4, name: "Riya", age: 21 }
+// // // // ];
+
+// // // // let filterdata = users.filter( item => item.age > 23)
+
+// // // // console.log(filterdata)
+
+// // // let users = [
+// // //   { id: 1, name: "Rahul", age: 22 },
+// // //   { id: 2, name: "Amit", age: 25 },
+// // //   { id: 3, name: "Neha", age: 24 },
+// // //   { id: 4, name: "Riya", age: 21 }
+// // // ];
+
+// // // let filterdata = users.filter(item => item.age > 23)
+
+// // // let usersName = filterdata.map(name => name.name)
+
+// // // console.log(filterdata , usersName)
+
+// // let numbers = [1,2,2,3,4,4,5,6,6];
+
+// // const newArray = new Set  (numbers)
+
+// // const uniqueset = [...newArray]
+
+// // console.log(uniqueset)
+
+// let users = [
+//   { id: 1, name: "Rahul", isActive: true },
+//   { id: 2, name: "Amit", isActive: false },
+//   { id: 3, name: "Neha", isActive: true },
+//   { id: 4, name: "Riya", isActive: false }
+// ];
+
+// let activeUsers = users.filter( user => user.isActive === true)
+// console.log(activeUsers)
+
